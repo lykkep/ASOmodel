ef68bf7554764f7feac5d6c580a5d90a44375621
Lykke Pedersen <lykkep@gmail.com>
2013-05-23 13:34:46 +0200

    New

---
+++ R/IC50FUNCTIONS.R
@@ -0,0 +1,98 @@
+###############################################################################
+#### Calculation of EC50
+###############################################################################
+#' @title Calculates the EC50 value at steady-state
+#' 
+#' @description
+#' Use the dose-respons curve to calculate the EC50 value 
+#' concentration before the addition of oligonucleotide. 
+#' 
+#' @param KdOT The dissociation constant of the OT complex in nM
+#' @param parms list of parameters
+#' 
+#' @return returns the EC50 value
+#' @examples 
+#' EC50(1,c(Et = 1,KdOT = 0.3,kOpT = 0.2,KdOTE = 70,
+#' kOTpE = 5,vprod = 0.2,vdegrad = 0.04,alpha=0.1,kcleav = 8))
+#' @export
+EC50 <- function(KdOT,param=parms){ #
+  param['KdOT'] <- KdOT
+  Otseq <- 10^seq(-3,3,length=50)
+  Trelseq <- Trel(Otseq,param=param)
+  parms  <- coefficients(drm(Trelseq~Otseq,fct=LL.5()))
+  out <- exp((parms[1]*log(parms[4])+
+                log(exp(log((2*(-parms[3]+parms[2]))/(parms[2]-1))/parms[5])
+                    -1))/parms[1]) 
+  names(out) <- 'EC50'
+  out
+}
+
+###############################################################################
+#### Calculation of EC50 for a stochastic simulation of the ASO model
+###############################################################################
+#' @title Calculates the EC50 value for a stochastic simulation of the ASO model
+#' 
+#' @description
+#' Use the stochastic dose-respons curve to calculate the EC50 value 
+#' concentration before the addition of oligonucleotide. 
+#' 
+#' @param Trel Sequence of stochastic simulated total target levels as output from \code{Trelstoc}
+#' @param Ot Sequence of total oligonucleotide concentrations used to calculate Trel
+#' 
+#' @return returns the EC50 value
+#' @examples 
+#' parms1 <- c(kOpT = 2E-5,kOTpE =50E-5 ,vprod = 150,  vdegrad = 0.04,  	  
+#' kcleav = 2, kOT =0.06, kOTE=2, kC = 0.1)
+#' #Initital state vector
+#' x0 <- c(Tt=parms1["vprod"]/parms1["vdegrad"],
+#'              OT=0,OTE=0,E=1e3,O=1e5,OCE=0,OC=0)
+#' names(x0) <- c('Tt','OT','OTE','E','O','OCE','OC')
+#' #Propensity vector
+#' a <-  c("vprod","kOpT*O*Tt","vdegrad*Tt","kOT*OT","kOTE*OTE","vdegrad*OT",
+#'          "kOTpE*OT*E","vdegrad*OTE","kcleav*OTE","kC*OC","kOTE*OCE" )
+#' #State-change matrix
+#' nu <- matrix(0,7,length(a))
+#' dimnames(nu) <- list(names(x0),a)
+#' nu['Tt',c('vprod','kOT*OT')] <- 1
+#' nu['Tt',c('kOpT*O*Tt','vdegrad*Tt')] <- -1 
+#' nu['OT',c('kOpT*O*Tt','kOTE*OTE')] <- 1
+#' nu['OT',c('kOT*OT','kOTpE*OT*E','vdegrad*OT')] <- -1
+#' nu['OTE',c('kOTpE*OT*E')] <- 1
+#' nu['OTE',c('kOTE*OTE','vdegrad*OTE','kcleav*OTE')] <- -1
+#' nu['E',c('kOTE*OTE','vdegrad*OTE','kOTE*OCE')] <- 1
+#' nu['E',c('kOTpE*OT*E')] <- -1
+#' nu['O',c('kOT*OT','vdegrad*OTE','vdegrad*OT','kC*OC')] <- 1
+#' nu['O',c('kOpT*O*Tt')] <- -1
+#' nu['OCE',c('kcleav*OTE')] <- 1
+#' nu['OCE',c('kOTE*OCE')] <- -1
+#' nu['OC',c('kOTE*OCE')] <- 1
+#' nu['OC',c('kC*OC')] <- -1
+#' 
+#' Otseq <- 10^seq(2.5,6,by=0.2)
+#' Trelseq <-sapply(Otseq,function(i) Trelstoc(i)$Tstat)})
+#' EC50STOC <- EC50stoc(Trelseq, Otseq)
+#' @export
+EC50stoc <- function(Trel,Ot){ #
+parms  <- coefficients(drm(Trel~Ot,fct=LL.5()))
+  out <- exp((parms[1]*log(parms[4])+
+                log(exp(log((2*(-parms[3]+parms[2]))/(parms[2]-1))/parms[5])-
+                      1))/parms[1]) 
+  names(out) <- 'EC50'
+  out
+}
+
+###############################################################################
+#### Calculation of EC50 for no coupling of off-rates
+###############################################################################
+#' @export
+EC50NO <- function(KdOT,param=parmsNO){
+  param['KdOT'] <- KdOT
+  Otseq <- 10^seq(-3,3,length=50)
+  Trelseq <- TrelNO(Otseq,param=param)
+  parms  <- coefficients(drm(Trelseq~Otseq,fct=LL.5()))
+  #b <- parms[1]; c <- parms[2]; d <- parms[3]; e <- parms[4]; f <- parms[5]
+  out <- exp((parms[1]*log(parms[4])+log(exp(log((2*(-parms[3]+parms[2]))/(parms[2]-1))/parms[5])-1))/parms[1]) 
+  names(out) <- 'EC50'
+  out
+}
+
---
+++ R/MiscFUNCTIONS.R
@@ -0,0 +1,82 @@
+last <- function(x) { tail(x, n = 1) }
+parms <- c(Et = 1,KdOT = 0.3,kOpT = 0.2,KdOTE = 70,
+           kOTpE = 5,vprod = 0.2,vdegrad = 0.04,alpha=0.1,kcleav = 8)
+
+###############################################################################
+#### The ODEs of the ASO model
+###############################################################################
+#TODO: document this function
+#' @export
+diffASO <- function(t,y, param){
+  k1 =param['kOpT']; D1 = param['KdOT']; k2 = param['kOTpE']; Et <- param['Et']
+  D2 = param['KdOTE']; vt = param['vprod']; k4 = param['vdegrad']
+  alpha=param['alpha']; kE=param['kcleav'];
+  km1 = k1*D1; km2=k2*D2; k3 = km1/alpha
+  
+  Tt = y[1]; OT = y[2]; OTE= y[3]; E=y[4]; O= y[5]
+  OCE= y[6]; OC = y[7]
+  
+  F <- {}   
+  #1= T
+  F[1]= vt-k1*O*Tt-k4*Tt+km1*OT
+  #2= OT
+  F[2]= k1*Tt*O-km1*OT-k2*OT*E+km2*OTE-k4*OT
+  #3= OTE
+  F[3]= k2*OT*E-km2*OTE-k4*OTE-kE*OTE
+  #4= E
+  F[4]= -k2*(OT)*E+km2*(OTE+OCE)+k4*(OTE)
+  #5= O
+  F[5]=km1*OT-k1*Tt*O+k4*(OT+OTE)+k3*OC
+  #OCE
+  F[6] = kE*OTE-km2*OCE-k3*OCE
+  #OC
+  F[7]= +km2*OCE-k3*OC
+  
+  return(list(F))
+}
+
+###############################################################################
+#### Plot dose-response curves for various parameters
+###############################################################################
+#TODO: document
+#' @export
+plot.doseresponse <- function(par,list.par,title,unit,plot=T){
+  if(plot){
+  curve(Trel,1E-2,1E4,log='x', 
+        xlab=expression(Total~oligo~concentration~'(nM)'),
+        ylab=expression(T[rel]), 
+        las=1,ylim=c(0,1),xaxt='n',mgp=c(2.2,0.7,0))
+  axis(1,at=10^c(-2,0,2,4),labels=pretty10expLP(10^c(-2,0,2,4),drop.1=T))
+  for(i in 1:length(list.par)){
+    parmsM <- parms ; parmsM[par] <- list.par[i]
+    runM <- function(x)Trel(x,param=parmsM)
+    curve(runM,1E-2,1E4,col=rainbow(length(list.par))[i],add=TRUE)
+  }
+  Legend=as.expression(sapply(c(parms[par],list.par),
+                              function(x) substitute(x*y,list(x=x,y=unit))))
+  legend("bottomleft", lwd=2, col=c('black',rainbow(length(list.par))),
+         legend=Legend, bty="n",cex=0.6)
+  legend(2E-3,0.6,title,bty='n',yjust=0.55,cex=0.7)
+  }
+}
+
+###############################################################################
+#### Pretty logarithmic axis-labels
+###############################################################################
+#TODO: document
+#' @export
+pretty10expLP <-  function (x, drop.1 = FALSE, digits.fuzz = 7) 
+{
+  eT <- floor(log10(abs(x)) + 10^-digits.fuzz)
+  mT <- signif(x/10^eT, digits.fuzz)
+  ss <- vector("list", length(x))
+  for (i in seq(along = x)) ss[[i]] <- if (x[i] == 0) 
+    quote(0)
+  else if (drop.1 && mT[i] == 1) 
+    substitute(10^E, list(E = eT[i]))
+  else if (drop.1 && mT[i] == -1) 
+    substitute(-10^E, list(E = eT[i]))
+  else substitute(A %.% 10^E, list(A = mT[i], E = eT[i]))
+  do.call("expression", ss)
+}
+
